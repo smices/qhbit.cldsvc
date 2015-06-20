0ddff25d5075e312cfc11980fc0f18acec37aaaa
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-06-20 15:35:27 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@7 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/xbspeed.php
+++ app/config/ServiceControl/xbspeed.php
@@ -10,6 +10,7 @@ $cf['version'] = '20150630';
 //*--------------------------------------------------------*/
 $cf['files'][] = [
     'name'          => 'XIAOBAI_WIN7_X86_V123.iso',  //要分享的文件名
+    'storage'       => '', //存放文件的目录名,文件存于??盘根目录
     'size'          => 12345315, //文件尺寸 ??Byte
     'hash'          => 'adsfasdf32dsafsffaasdfs23af', //文件md5哈希值, 文件Hash和Size都不对的情况下, 表明文件损坏
     'uploadSpeed'   => 15*1024,  //上传速度控制 ??Byte
@@ -19,6 +20,7 @@ $cf['files'][] = [
 //------------------//
 $cf['files'][] = [
     'name'          => 'XIAOBAI_WIN7_X64_V123.iso',
+    'storage'       => '', //存放文件的目录名,文件存于??盘根目录
     'size'          => 1234523424,
     'hash'          => 'adsfasdf32dsafsffaasdfs23af', //文件md5哈希值
     'uploadSpeed'   => 15*1024,
@@ -28,6 +30,7 @@ $cf['files'][] = [
 //------------------//
 $cf['files'][] = [
     'name'          => 'XIAOBAI_WIN8.1_X64_V456.iso',
+    'storage'       => '', //存放文件的目录名,文件存于??盘根目录
     'size'          => 1234523424,
     'hash'          => 'adsfasdf32dsafsffaasdfs23af', //文件md5哈希值
     'uploadSpeed'   => 15*1024,
