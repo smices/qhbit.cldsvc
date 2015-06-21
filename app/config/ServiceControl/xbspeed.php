9f0a92541b6a45abade7fe2986e37a0b59bfd929
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-06-21 14:28:12 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@22 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/xbspeed.php
+++ app/config/ServiceControl/xbspeed.php
@@ -9,13 +9,13 @@ $cf['version'] = 20150630;
 // 配置项
 //*--------------------------------------------------------*/
 $cf['files'][] = [
-    'name'          => 'XIAOBAI_WIN7_X86_V123.iso',  //要分享的文件名
+    'name'          => 'SD_win764_Ghost_CJ_SP1_2015_b0061ad.iso.iso',  //要分享的文件名
     'storage'       => '', //存放文件的目录名,文件存于??盘根目录
-    'size'          => 12345315, //文件尺寸 ??Byte
-    'hash'          => 'adsfasdf32dsafsffaasdfs23af', //文件md5哈希值, 文件Hash和Size都不对的情况下, 表明文件损坏
+    'size'          => 3984936960, //文件尺寸 ??Byte
+    'hash'          => '74df9cb54b1d1a02918e6de9ee53736a', //文件md5哈希值, 文件Hash和Size都不对的情况下, 表明文件损坏
     'uploadSpeed'   => 15*1024,  //上传速度控制 ??Byte
     'downloadSpeed' => 100,      //下载速度控制 ??%  (目前无效)
-    'downloadUrl'   => 'http://xxxx.com/dddd.asdfa.iso', //原始下载地址
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/SD/SD_win764_Ghost_CJ_SP1_2015_b0061ad.iso.iso', //原始下载地址
 ];
 //------------------//
 $cf['files'][] = [
