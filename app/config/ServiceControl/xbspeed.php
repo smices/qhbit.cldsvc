692c5c0b91a4d8e80687a5bd2a453c721294d481
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-06-22 02:32:33 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@24 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/xbspeed.php
+++ app/config/ServiceControl/xbspeed.php
@@ -3,39 +3,441 @@
 // XBSpeed 服务控制配置
 //*--------------------------------------------------------*/
 $cf = ["files"=>[]];
-$cf['version'] = 20150621;
+$cf['version'] = 20150622;
 
 //*--------------------------------------------------------*/
 // 配置项
 //*--------------------------------------------------------*/
+/***** 实例说明
 $cf['files'][] = [
     'name'          => 'SD_win764_Ghost_CJ_SP1_2015_b0061ad.iso.iso',  //要分享的文件名
-    'storage'       => '', //存放文件的目录名,文件存于??盘根目录
+    'storage'       => 'SDGHOST', //存放文件的目录名,文件存于??盘根目录
     'size'          => 3984936960, //文件尺寸 ??Byte
     'hash'          => '74df9cb54b1d1a02918e6de9ee53736a', //文件md5哈希值, 文件Hash和Size都不对的情况下, 表明文件损坏
     'uploadSpeed'   => 15*1024,  //上传速度控制 ??Byte
     'downloadSpeed' => 100,      //下载速度控制 ??%  (目前无效)
     'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/SD/SD_win764_Ghost_CJ_SP1_2015_b0061ad.iso.iso', //原始下载地址
 ];
+********/
+
 //------------------//
 $cf['files'][] = [
-    'name'          => 'XIAOBAI_WIN7_X64_V123.iso',
-    'storage'       => '', //存放文件的目录名,文件存于??盘根目录
-    'size'          => 1234523424,
-    'hash'          => 'adsfasdf32dsafsffaasdfs23af', //文件md5哈希值
+    'name'          => 'GHOSTxp_MFZ_2015006ca1.GHO',
+    'storage'       => 'MFZGhost',
+    'size'          => 1054672423,
+    'hash'          => 'bc9ae2069a9bc43cd9b80b1a5431f5ce',
     'uploadSpeed'   => 15*1024,
     'downloadSpeed' => 100,
-    'downloadUrl'   => 'http://xxxx.com/dddd.asdfa.iso', //原始下载地址
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/xp/GHOSTxp_MFZ_2015006ca1.GHO',
 ];
 //------------------//
+
 $cf['files'][] = [
-    'name'          => 'XIAOBAI_WIN8.1_X64_V456.iso',
-    'storage'       => '', //存放文件的目录名,文件存于??盘根目录
-    'size'          => 1234523424,
-    'hash'          => 'adsfasdf32dsafsffaasdfs23af', //文件md5哈希值
+    'name'          => 'GHOSTxp_MFZ_201500422.GHO',
+    'storage'       => 'MFZGhost',
+    'size'          => 1238929385,
+    'hash'          => 'b32f1f36d5d6f905af9a4617bc7a2672',
     'uploadSpeed'   => 15*1024,
     'downloadSpeed' => 100,
-    'downloadUrl'   => 'http://xxxx.com/dddd.asdfa.iso', //原始下载地址
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/xp/GHOSTxp_MFZ_201500422.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'GHOSTxp_MFZ_20150x07ca.iso',
+    'storage'       => 'MFZGhost',
+    'size'          => 1191047168,
+    'hash'          => '7031fdcd302fc45a03fd7c727654a173',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/xp/GHOSTxp_MFZ_20150x07ca.iso',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_MFZ_0525_ok.ORM',
+    'storage'       => 'MFZGhost',
+    'size'          => 4181151079,
+    'hash'          => '6dba69518984d5127fd7b1942600c98f',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0525_ok.ORM',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_MFZ_0521.ORM',  
+    'storage'       => 'MFZGhost',
+    'size'          => 3962523031,
+    'hash'          => 'b3cf97a7717064d39b134195bb2af30e',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0521.ORM',
+];
+//------------------//
+
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_MFZ_0525_ok.GHO',  
+    'storage'       => 'MFZGhost',
+    'size'          => 4182892369,
+    'hash'          => '0663230d461d365f79114e50c674b946',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0525_ok.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_MFZ_0525_ok.ISO', 
+    'storage'       => 'MFZGhost',
+    'size'          => 4318781440,
+    'hash'          => '2db461d8a79dd7514c0aa9ca93351360',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0525_ok.ISO',
+];
+//------------------//
+
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_MFZ_0521.gho', 
+    'storage'       => 'MFZGhost',
+    'size'          => 4094053712,
+    'hash'          => 'a5ea6f5cf18f920083f22a5ca0171543',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0521.gho',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8_000b05mf.GHO',  
+    'storage'       => 'MFZGhost',
+    'size'          => 3987504728,
+    'hash'          => 'e9c6c4494e1c231404d3c0c83c9f24d8',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8_000b05mf.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x64_MFZ_2015_0427.ORM',
+    'storage'       => 'MFZGhost',
+    'size'          => 3760669464,
+    'hash'          => '910775fc1a6b5900e2c69d9717d68c1b',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015_0427.ORM',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x86_MFZ_2015_0427.GHO',  
+    'storage'       => 'MFZGhost',
+    'size'          => 3145174914,
+    'hash'          => 'de8c48fe1bcaaa7bfbc6d30108d08b08',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x86_MFZ_2015_0427.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x64_MFZ_2015b0701ad.iso',  
+    'storage'       => 'MFZGhost',
+    'size'          => 4206891008,
+    'hash'          => 'd91de70421cd46a478a7dc221220c607',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015b0701ad.iso',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x86_MFZ_2015b0601ad.GHO',  
+    'storage'       => 'MFZGhost',
+    'size'          => 3066473878,
+    'hash'          => 'ab75c7c209b66a73e822505c9351403b',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x86_MFZ_2015b0601ad.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x64_MFZ_2015_0427.GHO',  
+    'storage'       => 'MFZGhost',
+    'size'          => 3975407457,
+    'hash'          => '32646594c2d51f62c43e212951caeac9',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015_0427.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x86_MFZ_2015b0701ad.iso',  
+    'storage'       => 'MFZGhost',
+    'size'          => 3201595392,
+    'hash'          => '2a02517e301943977636fc47c8f41f75',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x86_MFZ_2015b0701ad.iso',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'Win7_x64_MFZ_2015b0601ad.GHO',
+    'storage'       => 'MFZGhost',
+    'size'          => 4070536119,
+    'hash'          => '2d63c037960154f76cceb63c00a139c7',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015b0601ad.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'xiaobai_winXPxb06c1.GHO',
+    'storage'       => 'XBGhost',
+    'size'          => 1054159298,
+    'hash'          => 'b88b8e4bbb34655a7663760d529d00f4',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/xp/xiaobai_winXPxb06c1.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'xiaobai_winXP150421.GHO',
+    'storage'       => 'XBGhost',
+    'size'          => 1239036258,
+    'hash'          => '61105f68cb6721fa6dab5fdd955e04c6',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/xp/xiaobai_winXP150421.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8_1XB00501.GHO',  
+    'storage'       => 'XBGhost',
+    'size'          => 3987819538,
+    'hash'          => 'b680aa6043f74fdcc25580271ab7e743',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8_1XB00501.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win.8.1_x64_xiaobai_CZ_1506.ORM',  
+    'storage'       => 'XBGhost',
+    'size'          => 4181198527,
+    'hash'          => '2da522528adff73a9d4f506235cadf33',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win.8.1_x64_xiaobai_CZ_1506.ORM',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win.8.1_x64_xiaobai_CZ_1506.GHO',  
+    'storage'       => 'XBGhost',
+    'size'          => 4183098646,
+    'hash'          => '052d4cf5f37825330eb910cd535b135b',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win.8.1_x64_xiaobai_CZ_1506.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_xiaobai_0525_ok.ORM',  
+    'storage'       => 'XBGhost',
+    'size'          => 4181198527,
+    'hash'          => '2da522528adff73a9d4f506235cadf33',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8.1_x64_xiaobai_0525_ok.ORM',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x64_xiaobai_0525_ok.GHO',  
+    'storage'       => 'XBGhost',
+    'size'          => 4183098646,
+    'hash'          => '052d4cf5f37825330eb910cd535b135b',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8.1_x64_xiaobai_0525_ok.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win8.1_x32_xiaobai_0526_ok.gho',
+    'storage'       => 'XBGhost',
+    'size'          => 3326930207,
+    'hash'          => '5af83f615c7cad3a0a07fe47b19d6c77',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8.1_x32_xiaobai_0526_ok.gho',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'xiaobai_baidu_64_2015_0423.GHO',  
+    'storage'       => 'XBGhost',
+    'size'          => 3976359407,
+    'hash'          => '40f2e139ce45e01d0b74f55dd13d9bef',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/xiaobai_baidu_64_2015_0423.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'xiaobai_bd_win764_2015_bc06a2.GHO',
+    'storage'       => 'XBGhost',
+    'size'          => 4069946043,
+    'hash'          => '6eec56fb3fb075fb99dd4ceb22dada8c',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/xiaobai_bd_win764_2015_bc06a2.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'xiaobai_bd_win786_2015_bc06a2.GHO',
+    'storage'       => 'XBGhost',
+    'size'          => 3066106620,
+    'hash'          => '0b5df6216bfb654dd274d93e066bf75a',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/xiaobai_bd_win786_2015_bc06a2.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'XBwin7_x86_1504025.GHO',
+    'storage'       => 'XBGhost',
+    'size'          => 3145956277,
+    'hash'          => '9fc63a02937ccdfcfd980aba80c91d00',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/XBwin7_x86_1504025.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'xtzj_WINxp_BC0601.GHO',
+    'storage'       => 'ZJGhost',
+    'size'          => 1054758152,
+    'hash'          => '8c78dd1f4f235bb6d4318ece0708f649',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/xp/xtzj_WINxp_BC0601.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'SD_windowszj_8.1_32_CJ06.iso',
+    'storage'       => 'ZJGhost',
+    'size'          => 3158290432,
+    'hash'          => '0b6c676c798910d6b5275632db492ca3',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win832/SD_windowszj_8.1_32_CJ06.iso',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win7_ZJ_x64_CZ2015b060B2.GHO',
+    'storage'       => 'ZJGhost',
+    'size'          => 3826428044,
+    'hash'          => '21f759f18dc5dea2505c099d414f4f4f',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win764/win7_ZJ_x64_CZ2015b060B2.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'SD_windowszj_8.1_64_CJ06.iso.ORM',
+    'storage'       => 'ZJGhost',
+    'size'          => 3737647606,
+    'hash'          => 'ce748ec1546826356eadd48c06b92431',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win864/SD_windowszj_8.1_64_CJ06.iso.ORM',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'WIN8.1_X64_ZJ_CJ_0531_OK.ORM',
+    'storage'       => 'ZJGhost',
+    'size'          => 3737647606,
+    'hash'          => 'ce748ec1546826356eadd48c06b92431',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win864/WIN8.1_X64_ZJ_CJ_0531_OK.ORM',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'SD_windowszj_8.1_64_CJ06.iso.iso',
+    'storage'       => 'ZJGhost',
+    'size'          => 3994546176,
+    'hash'          => 'd8182bb312a45b9c80dc7946883a3725',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win864/SD_windowszj_8.1_64_CJ06.iso.iso',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'SD_windowszj_8.1_64_CJ06.iso.GHO',  
+    'storage'       => 'ZJGhost',
+    'size'          => 3812825896,
+    'hash'          => '3809a879eb04462aa2224ce547d7aac2',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win864/SD_windowszj_8.1_64_CJ06.iso.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'WIN8.1_X64_ZJ_CJ_0531_OK.GHO',
+    'storage'       => 'ZJGhost',
+    'size'          => 3812825896,
+    'hash'          => '3809a879eb04462aa2224ce547d7aac2',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win864/WIN8.1_X64_ZJ_CJ_0531_OK.GHO',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'SD_win786Ghost_CJ_SP1_2015_b0061ad4.iso',
+    'storage'       => 'ZJGhost',
+    'size'          => 3079409664,
+    'hash'          => '6a0a0801b56a4fe9a425a13e7b7ac94e',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win732/SD_win786Ghost_CJ_SP1_2015_b0061ad4.iso',
+];
+//------------------//
+
+$cf['files'][] = [
+    'name'          => 'win732/win7_ZJ_x86_CZ2015b060B2.GHO',  
+    'storage'       => 'ZJGhost',
+    'size'          => 2923105646,
+    'hash'          => '8530a45830b815d064e3f4dcd8ea6a1c',
+    'uploadSpeed'   => 15*1024,
+    'downloadSpeed' => 100,
+    'downloadUrl'   => 'http://down.windowszj.com/zj/win732/win7_ZJ_x86_CZ2015b060B2.GHO',
 ];
 //------------------//
 //------------------//
