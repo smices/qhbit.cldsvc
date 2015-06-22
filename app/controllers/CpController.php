d20be8a53426af51de446bce36a55b09832ec0b1
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-06-22 11:04:17 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@26 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/controllers/CpController.php
+++ app/controllers/CpController.php
@@ -8,10 +8,68 @@ class CpController extends ControllerSecurity
     }//end init
 
     /**
+     * 读取配置文件
+     * @param $svcName
+     *
+     * @return bool|\Phalcon\Config\Adapter\Php
+     */
+    public function getSvcConfig($svcName)
+    {
+        $cfFile = _DYP_DIR_CFG . '/ServiceControl/' . $svcName . '.php';
+        if (!is_file(realpath($cfFile))) {
+            return false;
+        } else {
+            $cfg = new Phalcon\Config\Adapter\Php(realpath($cfFile));
+            if ($cfg) return $cfg;
+        }
+    }//end
+
+    /**
      * Index
      */
     public function indexAction()
     {
+
+    }//end
+
+    /**
+     * xbSpeed Service
+     */
+    public function xbspeedAction(){
+        if($cfg = $this->getSvcConfig('xbspeed')){
+            $this->view->list = $cfg;
+        }else{
+            $this->flash->error('NOT FIND CONFIG FILE');
+        }
+    }//end
+
+    /**
+     * 核心服务管理
+     */
+    public function coresvcAction(){
+        if($cfg = $this->getSvcConfig('_upgrade')){
+            $this->view->list = $cfg;
+        }else{
+            $this->flash->error('NOT FIND CONFIG FILE');
+        }
+    }//end
+
+    /**
+     * 预装软件管理
+     */
+    public function presoftwareAction(){
+        if($cfg = $this->getSvcConfig('_software')){
+            $this->view->list = $cfg;
+        }else{
+            $this->flash->error('NOT FIND CONFIG FILE');
+        }
+    }//end
+
+    /**
+     * 数据分析管理
+     */
+    public function analysisAction(){
+
     }//end
 
 }//end
--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -14,9 +14,6 @@ class ErrorController extends ControllerBase
      */
     public function e404Action()
     {
-        $this->view->disable();
-		echo "<h1>HTTP 404 ERROR</h1>";
-        echo "<h2>Page not find.</h2>";
     }//end
 
 }//end
