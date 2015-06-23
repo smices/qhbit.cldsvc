7338f2b94ef5c2f53667e7d38bf90c5812fcad0f
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-06-23 07:27:48 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@34 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/xbspeed.php
+++ app/config/ServiceControl/xbspeed.php
@@ -28,7 +28,7 @@ $cf['files'][] = [
     'fileHash'      => 'bc9ae2069a9bc43cd9b80b1a5431f5ce',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/xp/GHOSTxp_MFZ_2015006ca1.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/GHOSTxp_MFZ_2015006ca1.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/GHOSTxp_MFZ_2015006ca1.GHO.td.cfg',
 ];
 //------------------//
 
@@ -39,7 +39,7 @@ $cf['files'][] = [
     'fileHash'      => 'b32f1f36d5d6f905af9a4617bc7a2672',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/xp/GHOSTxp_MFZ_201500422.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/GHOSTxp_MFZ_201500422.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/GHOSTxp_MFZ_201500422.GHO.td.cfg',
 ];
 //------------------//
 
@@ -50,7 +50,7 @@ $cf['files'][] = [
     'fileHash'      => '7031fdcd302fc45a03fd7c727654a173',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/xp/GHOSTxp_MFZ_20150x07ca.iso',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/GHOSTxp_MFZ_20150x07ca.iso.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/GHOSTxp_MFZ_20150x07ca.iso.td.cfg',
 ];
 //------------------//
 
@@ -61,7 +61,7 @@ $cf['files'][] = [
     'fileHash'      => '6dba69518984d5127fd7b1942600c98f',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0525_ok.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_MFZ_0525_ok.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_MFZ_0525_ok.ORM.td.cfg',
 ];
 //------------------//
 
@@ -72,7 +72,7 @@ $cf['files'][] = [
     'fileHash'      => 'b3cf97a7717064d39b134195bb2af30e',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0521.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_MFZ_0521.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_MFZ_0521.ORM.td.cfg',
 ];
 //------------------//
 
@@ -84,7 +84,7 @@ $cf['files'][] = [
     'fileHash'      => '0663230d461d365f79114e50c674b946',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0525_ok.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_MFZ_0525_ok.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_MFZ_0525_ok.GHO.td.cfg',
 ];
 //------------------//
 
@@ -95,7 +95,7 @@ $cf['files'][] = [
     'fileHash'      => '2db461d8a79dd7514c0aa9ca93351360',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0525_ok.ISO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_MFZ_0525_ok.ISO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_MFZ_0525_ok.ISO.td.cfg',
 ];
 //------------------//
 
@@ -107,7 +107,7 @@ $cf['files'][] = [
     'fileHash'      => 'a5ea6f5cf18f920083f22a5ca0171543',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8.1_x64_MFZ_0521.gho',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_MFZ_0521.gho.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_MFZ_0521.gho.td.cfg',
 ];
 //------------------//
 
@@ -118,7 +118,7 @@ $cf['files'][] = [
     'fileHash'      => 'e9c6c4494e1c231404d3c0c83c9f24d8',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win8/win8_000b05mf.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8_000b05mf.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8_000b05mf.GHO.td.cfg',
 ];
 //------------------//
 
@@ -129,7 +129,7 @@ $cf['files'][] = [
     'fileHash'      => '910775fc1a6b5900e2c69d9717d68c1b',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015_0427.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x64_MFZ_2015_0427.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x64_MFZ_2015_0427.ORM.td.cfg',
 ];
 //------------------//
 
@@ -140,7 +140,7 @@ $cf['files'][] = [
     'fileHash'      => 'de8c48fe1bcaaa7bfbc6d30108d08b08',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x86_MFZ_2015_0427.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x86_MFZ_2015_0427.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x86_MFZ_2015_0427.GHO.td.cfg',
 ];
 //------------------//
 
@@ -151,7 +151,7 @@ $cf['files'][] = [
     'fileHash'      => 'd91de70421cd46a478a7dc221220c607',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015b0701ad.iso',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x64_MFZ_2015b0701ad.iso.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x64_MFZ_2015b0701ad.iso.td.cfg',
 ];
 //------------------//
 
@@ -162,7 +162,7 @@ $cf['files'][] = [
     'fileHash'      => 'ab75c7c209b66a73e822505c9351403b',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x86_MFZ_2015b0601ad.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x86_MFZ_2015b0601ad.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x86_MFZ_2015b0601ad.GHO.td.cfg',
 ];
 //------------------//
 
@@ -173,7 +173,7 @@ $cf['files'][] = [
     'fileHash'      => '32646594c2d51f62c43e212951caeac9',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015_0427.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x64_MFZ_2015_0427.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x64_MFZ_2015_0427.GHO.td.cfg',
 ];
 //------------------//
 
@@ -184,7 +184,7 @@ $cf['files'][] = [
     'fileHash'      => '2a02517e301943977636fc47c8f41f75',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x86_MFZ_2015b0701ad.iso',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x86_MFZ_2015b0701ad.iso.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x86_MFZ_2015b0701ad.iso.td.cfg',
 ];
 //------------------//
 
@@ -195,7 +195,7 @@ $cf['files'][] = [
     'fileHash'      => '2d63c037960154f76cceb63c00a139c7',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.mofazhu.com/mofazhu/win7/Win7_x64_MFZ_2015b0601ad.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/Win7_x64_MFZ_2015b0601ad.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x64_MFZ_2015b0601ad.GHO.td.cfg',
 ];
 //------------------//
 
@@ -206,7 +206,7 @@ $cf['files'][] = [
     'fileHash'      => 'b88b8e4bbb34655a7663760d529d00f4',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/xp/xiaobai_winXPxb06c1.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xiaobai_winXPxb06c1.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_winXPxb06c1.GHO.td.cfg',
 ];
 //------------------//
 
@@ -217,7 +217,7 @@ $cf['files'][] = [
     'fileHash'      => '61105f68cb6721fa6dab5fdd955e04c6',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/xp/xiaobai_winXP150421.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xiaobai_winXP150421.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_winXP150421.GHO.td.cfg',
 ];
 //------------------//
 
@@ -228,7 +228,7 @@ $cf['files'][] = [
     'fileHash'      => 'b680aa6043f74fdcc25580271ab7e743',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8_1XB00501.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8_1XB00501.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8_1XB00501.GHO.td.cfg',
 ];
 //------------------//
 
@@ -239,7 +239,7 @@ $cf['files'][] = [
     'fileHash'      => '2da522528adff73a9d4f506235cadf33',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win.8.1_x64_xiaobai_CZ_1506.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win.8.1_x64_xiaobai_CZ_1506.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win.8.1_x64_xiaobai_CZ_1506.ORM.td.cfg',
 ];
 //------------------//
 
@@ -250,7 +250,7 @@ $cf['files'][] = [
     'fileHash'      => '052d4cf5f37825330eb910cd535b135b',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win.8.1_x64_xiaobai_CZ_1506.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xxx.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xxx.td.cfg',
 ];
 //------------------//
 
@@ -261,7 +261,7 @@ $cf['files'][] = [
     'fileHash'      => '2da522528adff73a9d4f506235cadf33',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8.1_x64_xiaobai_0525_ok.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_xiaobai_0525_ok.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_xiaobai_0525_ok.ORM.td.cfg',
 ];
 //------------------//
 
@@ -272,7 +272,7 @@ $cf['files'][] = [
     'fileHash'      => '052d4cf5f37825330eb910cd535b135b',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8.1_x64_xiaobai_0525_ok.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x64_xiaobai_0525_ok.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x64_xiaobai_0525_ok.GHO.td.cfg',
 ];
 //------------------//
 
@@ -283,7 +283,7 @@ $cf['files'][] = [
     'fileHash'      => '5af83f615c7cad3a0a07fe47b19d6c77',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win8/win8.1_x32_xiaobai_0526_ok.gho',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win8.1_x32_xiaobai_0526_ok.gho.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win8.1_x32_xiaobai_0526_ok.gho.td.cfg',
 ];
 //------------------//
 
@@ -294,7 +294,7 @@ $cf['files'][] = [
     'fileHash'      => '40f2e139ce45e01d0b74f55dd13d9bef',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/xiaobai_baidu_64_2015_0423.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xiaobai_baidu_64_2015_0423.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_baidu_64_2015_0423.GHO.td.cfg',
 ];
 //------------------//
 
@@ -305,7 +305,7 @@ $cf['files'][] = [
     'fileHash'      => '6eec56fb3fb075fb99dd4ceb22dada8c',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/xiaobai_bd_win764_2015_bc06a2.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xiaobai_bd_win764_2015_bc06a2.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_bd_win764_2015_bc06a2.GHO.td.cfg',
 ];
 //------------------//
 
@@ -316,7 +316,7 @@ $cf['files'][] = [
     'fileHash'      => '0b5df6216bfb654dd274d93e066bf75a',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/xiaobai_bd_win786_2015_bc06a2.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xiaobai_bd_win786_2015_bc06a2.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_bd_win786_2015_bc06a2.GHO.td.cfg',
 ];
 //------------------//
 
@@ -327,7 +327,7 @@ $cf['files'][] = [
     'fileHash'      => '9fc63a02937ccdfcfd980aba80c91d00',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.xiaobaixitong.com/xiaobai/win7/XBwin7_x86_1504025.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/XBwin7_x86_1504025.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/XBwin7_x86_1504025.GHO.td.cfg',
 ];
 //------------------//
 
@@ -338,7 +338,7 @@ $cf['files'][] = [
     'fileHash'      => '8c78dd1f4f235bb6d4318ece0708f649',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/xp/xtzj_WINxp_BC0601.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/xtzj_WINxp_BC0601.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xtzj_WINxp_BC0601.GHO.td.cfg',
 ];
 //------------------//
 
@@ -349,7 +349,7 @@ $cf['files'][] = [
     'fileHash'      => '0b6c676c798910d6b5275632db492ca3',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win832/SD_windowszj_8.1_32_CJ06.iso',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/SD_windowszj_8.1_32_CJ06.iso.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_windowszj_8.1_32_CJ06.iso.td.cfg',
 ];
 //------------------//
 
@@ -360,7 +360,7 @@ $cf['files'][] = [
     'fileHash'      => '21f759f18dc5dea2505c099d414f4f4f',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win764/win7_ZJ_x64_CZ2015b060B2.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win7_ZJ_x64_CZ2015b060B2.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_ZJ_x64_CZ2015b060B2.GHO.td.cfg',
 ];
 //------------------//
 
@@ -371,7 +371,7 @@ $cf['files'][] = [
     'fileHash'      => 'ce748ec1546826356eadd48c06b92431',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win864/SD_windowszj_8.1_64_CJ06.iso.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/SD_windowszj_8.1_64_CJ06.iso.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_windowszj_8.1_64_CJ06.iso.ORM.td.cfg',
 ];
 //------------------//
 
@@ -382,7 +382,7 @@ $cf['files'][] = [
     'fileHash'      => 'ce748ec1546826356eadd48c06b92431',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win864/WIN8.1_X64_ZJ_CJ_0531_OK.ORM',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/WIN8.1_X64_ZJ_CJ_0531_OK.ORM.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/WIN8.1_X64_ZJ_CJ_0531_OK.ORM.td.cfg',
 ];
 //------------------//
 
@@ -393,7 +393,7 @@ $cf['files'][] = [
     'fileHash'      => 'd8182bb312a45b9c80dc7946883a3725',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win864/SD_windowszj_8.1_64_CJ06.iso.iso',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/SD_windowszj_8.1_64_CJ06.iso.iso.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_windowszj_8.1_64_CJ06.iso.iso.td.cfg',
 ];
 //------------------//
 
@@ -404,7 +404,7 @@ $cf['files'][] = [
     'fileHash'      => '3809a879eb04462aa2224ce547d7aac2',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win864/SD_windowszj_8.1_64_CJ06.iso.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/SD_windowszj_8.1_64_CJ06.iso.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_windowszj_8.1_64_CJ06.iso.GHO.td.cfg',
 ];
 //------------------//
 
@@ -415,7 +415,7 @@ $cf['files'][] = [
     'fileHash'      => '3809a879eb04462aa2224ce547d7aac2',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win864/WIN8.1_X64_ZJ_CJ_0531_OK.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/WIN8.1_X64_ZJ_CJ_0531_OK.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/WIN8.1_X64_ZJ_CJ_0531_OK.GHO.td.cfg',
 ];
 //------------------//
 
@@ -426,7 +426,7 @@ $cf['files'][] = [
     'fileHash'      => '6a0a0801b56a4fe9a425a13e7b7ac94e',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win732/SD_win786Ghost_CJ_SP1_2015_b0061ad4.iso',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/SD_win786Ghost_CJ_SP1_2015_b0061ad4.iso.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_win786Ghost_CJ_SP1_2015_b0061ad4.iso.td.cfg',
 ];
 //------------------//
 
@@ -437,7 +437,7 @@ $cf['files'][] = [
     'fileHash'      => '8530a45830b815d064e3f4dcd8ea6a1c',
     'uploadSpeed'   => 15,
     'downloadUrl'   => 'http://down.windowszj.com/zj/win732/win7_ZJ_x86_CZ2015b060B2.GHO',
-    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/xbspeed/win7_ZJ_x86_CZ2015b060B2.GHO.td.cfg',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_ZJ_x86_CZ2015b060B2.GHO.td.cfg',
 ];
 //------------------//
 //------------------//
