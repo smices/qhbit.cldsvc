47b3874926e80a5d15a0497d3db953749c9a9c90
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-06-24 08:40:17 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@36 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/_upgrade.php
+++ app/config/ServiceControl/_upgrade.php
@@ -3,7 +3,7 @@
 // XBSpeed 服务控制配置
 //*--------------------------------------------------------*/
 $cf = ["files"=>[]];
-$cf['version'] = 20150622;
+$cf['version'] = 20150624;
 
 //*--------------------------------------------------------*/
 // 配置项
@@ -40,14 +40,14 @@ $cf['files'][] = [
     'service'         => 'xbSpeed',     //服务名称
     'updateMode'      => 'install',       //当为install时表示要安装或更新到这个版本, 当为uninstall时, 表示这个包, 要执行自我消除
     'lastVersion'     => '1.0.0',         // 最新版本的版本号
-    'lastVersionCode' => 20150630,        //最新版本的版本代码,更新主要以这个Int号为准
-    'releaseTime'     => '2015/06/20',    // 最新版本发布时间
+    'lastVersionCode' => 20150624,        //最新版本的版本代码,更新主要以这个Int号为准
+    'releaseTime'     => '2015/06/24',    // 最新版本发布时间
     'lowCompatible'   => '5.1',           // (5.1) 最低兼容版本, 比如只兼容到win7, xp系统来更新, 将不会执行更新
     'arch'            => '32+64',         //[32|64|32+64] 标识系统架构, 32+64表示都支持,其他表示指定
     'fileName'        => 'xbSpeed.exe',   //文件真实名称
     'fileSize'        => 46553556,          //文件大小 Byte
-    'fileHash'        => '8679374d1a8ccfd7f89c37d2e69f9ea5',  //文件MD5摘要
-    'downloadUrl'     => 'http://ctr.datacld.com/release/'.$cf['version'].'/xbSpeed.upkg',//文件下载地址
+    'fileHash'        => '934afb5c84892dde5274bd4dc3f0bbff',  //文件MD5摘要
+    'downloadUrl'     => 'http://ctr.datacld.com/fs/svc/xbspeed/upgrade/20150624.upkg',//文件下载地址
 ];
 //------------------//
 $cf['files'][] = [
