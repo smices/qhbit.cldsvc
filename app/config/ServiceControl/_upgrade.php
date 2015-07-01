a484a6aa2a1a48fdff51beeabdd731acf5600fa6
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-07-01 01:50:15 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@58 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/_upgrade.php
+++ app/config/ServiceControl/_upgrade.php
@@ -3,7 +3,7 @@
 // XBSpeed 服务控制配置
 //*--------------------------------------------------------*/
 $cf = ["files"=>[]];
-$cf['version'] = 20150635;
+$cf['version'] = 20150701;
 
 //*--------------------------------------------------------*/
 // 配置项
@@ -39,15 +39,15 @@ $cf['files'][] = [
 $cf['files'][] = [
 'service'         => 'xbSpeed',
 'updateMode'      => 'install',
-'lastVersion'     => '1.3.0.6',
-'lastVersionCode' => 20150630,
-'releaseTime'     => '2015/06/30',
+'lastVersion'     => '1.3.0.7',
+'lastVersionCode' => 20150701,
+'releaseTime'     => '2015/07/01',
 'lowCompatible'   => '5.1',
 'arch'            => '32+64',
 'fileName'        => 'xbSpeed.exe',
 'fileSize'        => 411069,
-'fileHash'        => '7e47444ce95f078b4149ba948a03f382',
-'downloadUrl'     => 'http://ctr.datacld.com/fs/svc/xbspeed/upgrade/20150630.upkg',
+'fileHash'        => '3f5e75c65b2f48cd38c514b6b08feb1d',
+'downloadUrl'     => 'http://ctr.datacld.com/fs/svc/xbspeed/upgrade/20150701.upkg',
 ];
 //------------------//
 $cf['files'][] = [
