67705306fb215bd7c513b1b8734daa42fa04330a
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-07-06 13:09:34 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@73 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/config/ServiceControl/xbspeed.php
+++ app/config/ServiceControl/xbspeed.php
@@ -3,7 +3,7 @@
 // XBSpeed 服务控制配置
 //*--------------------------------------------------------*/
 $cf = ["files"=>[]];
-$cf['version'] = 20150707;
+$cf['version'] = 20150708;
 
 //*--------------------------------------------------------*/
 // 配置项
@@ -28,7 +28,7 @@ $cf['files'][] = [
     'fileSize'      => 4003452928,
     'fileHash'      => 'd56a52cc71f493a15dcb6f6f1d00f998',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X64_MFZ_CJ_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X64_MFZ_CJ_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X64_MFZ_CJ_0701.iso.td.cfg',
 ];
 //------------------//
@@ -39,7 +39,7 @@ $cf['files'][] = [
     'fileSize'      => 4241733632,
     'fileHash'      => 'fc2c2c9cc206cc250f27a6d61accc987',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X64_XB__0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X64_XB__0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X64_XB__0701.iso.td.cfg',
 ];
 //------------------//
@@ -53,7 +53,7 @@ $cf['files'][] = [
     'fileSize'      => 4003657728,
     'fileHash'      => '22bdc2683ebe770234b91b3576588ef7',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X64_XB_CJ_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X64_XB_CJ_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X64_XB_CJ_0701.iso.td.cfg',
 ];
 //------------------//
@@ -64,7 +64,7 @@ $cf['files'][] = [
     'fileSize'      => 4003880960,
     'fileHash'      => 'd99eab5e8b4d60453608faf1f491475d',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X64_ZJ_CJ_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X64_ZJ_CJ_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X64_ZJ_CJ_0701.iso.td.cfg',
 ];
 //------------------//
@@ -75,7 +75,7 @@ $cf['files'][] = [
     'fileSize'      => 3303636992,
     'fileHash'      => 'ce8a7ee054ec86ee8d0ea0dabb0b2417',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X86_ZJ_CJ_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X86_ZJ_CJ_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X86_ZJ_CJ_0701.iso.td.cfg',
 ];
 //------------------//
@@ -88,7 +88,7 @@ $cf['files'][] = [
     'fileSize'      => 3303208960,
     'fileHash'      => '32aaf662723a2a8ae6f6100069a8dd37',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X32_MFZ_CJ_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X32_MFZ_CJ_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X32_MFZ_CJ_0701.iso.td.cfg',
 ];
 //------------------//
@@ -99,7 +99,7 @@ $cf['files'][] = [
     'fileSize'      => 3411976192,
     'fileHash'      => '5a9de9d0a1308852573740d3d67fe90b',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X32_XB_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X32_XB_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X32_XB_0701.iso.td.cfg',
 ];
 //------------------//
@@ -109,11 +109,133 @@ $cf['files'][] = [
     'fileSize'      => 3303403520,
     'fileHash'      => 'cea45f93018b0766048910138607d869',
     'uploadSpeed'   => 15,
-    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/Win8.1_X32_XB_CJ_0701.iso',
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win8/201507/Win8.1_X32_XB_CJ_0701.iso',
     'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win8.1_X32_XB_CJ_0701.iso.td.cfg',
 ];
 //------------------//
 
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'Win7_x86_MFZ_2015b0702ad.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 3331332096,
+    'fileHash'      => '3c061396ded8f9a40aea6f178d9e7a42',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win732/Win7_x86_MFZ_2015b0702ad.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x86_MFZ_2015b0702ad.iso.td.cfg',
+];
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'win7_x86ZJ_CZ2015b070a2.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 3133052928,
+    'fileHash'      => 'dcb1d1ece34e25430db2d89a38918602',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win732/win7_x86ZJ_CZ2015b070a2.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_x86ZJ_CZ2015b070a2.iso.td.cfg',
+];
+//------------------//
+
+
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'win7_Ylmf_Win7_x86_CZ201500073a.iso',
+    'storage'       => '', //任意目录 或迅雷下载目录
+    'fileSize'      => 3133995008 ,
+    'fileHash'      => 'c92d2a1b7cd831c1b85a661bc3ad0963',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win732/win7_Ylmf_Win7_x86_CZ201500073a.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_Ylmf_Win7_x86_CZ201500073a.iso.td.cfg',
+];
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'xiaobai_CZ_win764_2015_bc07a2.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 4146866176,
+    'fileHash'      => '54be2e0b5792cfba1ab1bb592533070f',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/xiaobai_CZ_win764_2015_bc07a2.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_CZ_win764_2015_bc07a2.iso.td.cfg',
+];
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'xiaobai_CZ_win786_2015_bc07a2.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 3331153920,
+    'fileHash'      => 'f5fd28d7f51736b6709f82ee16bf3514',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win786/xiaobai_CZ_win786_2015_bc07a2.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/xiaobai_CZ_win786_2015_bc07a2.iso.td.cfg',
+];
+//------------------//
+
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'SD_win764_Ghost_CJ_SP1_2015_b007a2s.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 3996440576,
+    'fileHash'      => '205f6f95be033eb5327d917e3b02ab54',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/SD_win764_Ghost_CJ_SP1_2015_b007a2s.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_win764_Ghost_CJ_SP1_2015_b007a2s.iso.td.cfg',
+];
+//------------------//
+
+$cf['files'][] = [
+    'fileName'      => 'SD_win786Ghost_CJ_SP1_2015_b007a1.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 3128827904,
+    'fileHash'      => '989e5e9094e718e74ce7d80dda75bf3f',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win732/SD_win786Ghost_CJ_SP1_2015_b007a1.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/SD_win786Ghost_CJ_SP1_2015_b007a1.iso.td.cfg',
+];
+//------------------//
+$cf['files'][] = [
+    'fileName'      => 'win7_Luobo_x64CJ_2015_07d2a.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 4002361344,
+    'fileHash'      => '765a63a109acdc8da2f6ec2008177374',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/win7_Luobo_x64CJ_2015_07d2a.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_Luobo_x64CJ_2015_07d2a.iso.td.cfg',
+];
+//------------------//
+$cf['files'][] = [
+    'fileName'      => 'win7_Luobo_x86CJ_2015_07d2a.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 3125149696,
+    'fileHash'      => '90fe1d02932a07016201d1d5be46d868',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win786/win7_Luobo_x86CJ_2015_07d2a.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_Luobo_x86CJ_2015_07d2a.iso.td.cfg',
+];
+//------------------//
+$cf['files'][] = [
+    'fileName'      => 'Win7_x64_MFZ_2015b0701ad.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 4146995200,
+    'fileHash'      => '4fc0258686b3001d378007990a0b9d0c',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/Win7_x64_MFZ_2015b0701ad.isoo',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/Win7_x64_MFZ_2015b0701ad.iso.td.cfg',
+];
+//------------------//
+$cf['files'][] = [
+    'fileName'      => 'win7_x64_ZJ_CZ2015b070a2.iso',
+    'storage'       => '',//任意目录 或迅雷下载目录
+    'fileSize'      => 4009930752,
+    'fileHash'      => '8d8b82e57ce81c0df634527e30c9e829',
+    'uploadSpeed'   => 15,
+    'downloadUrl'   => 'http://7en.mofazhu.com:808/win764/win7_x64_ZJ_CZ2015b070a2.iso',
+    'tdConfigUrl'   => 'http://ctr.datacld.com/fs/svc/xbspeed/tdConfigUrl/win7_x64_ZJ_CZ2015b070a2.iso.td.cfg',
+];
 
 //------------------//
 //------------------//
