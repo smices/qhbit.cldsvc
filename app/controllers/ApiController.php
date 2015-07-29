48fe7eb45d7abe2efeb78f3a89b806bca61e08e4
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-07-29 04:20:35 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@128 ffb7e7ae-e771-4db6-8443-a1b56181394b

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -281,6 +281,44 @@ class ApiController extends ControllerApi
 
 
     /**
+     * 用户注册
+     */
+    public function userRegister(){
+        if($this->request->isGet()){
+            /**
+             * 用户注册状态检测
+             */
+            $type = $this->request->getQuery('type', 'string', null);
+            if(null == $type) Resp::outJsonMsg(1, 'TYPE NOT MATCH');
+
+        }elseif($this->request->isPost()){
+            /**
+             * 响应用户注册
+             */
+            Resp::outJsonMsg(1, 'TYPE NOT MATCH');
+        }
+    }//end
+
+
+    /**
+     * 用户信息管理
+     */
+    public function userAction(){
+        if($this->request->isGet()){
+            /**
+             * 获取用户登录状态等相关内容
+             * 如果没有登录, 则返回失败
+             */
+        }elseif($this->request->isPost()){
+            /**
+             * 更新用户内容
+             */
+        }
+
+    }//end
+
+
+    /**
      * 汇报
      */
     public function reportAction(){
