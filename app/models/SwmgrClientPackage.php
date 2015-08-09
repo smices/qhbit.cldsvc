a7bcdb15bc244db160d18a0faf1dc8fc6500aada
jacky <jacky@ffb7e7ae-e771-4db6-8443-a1b56181394b>
2015-08-09 09:57:49 +0000

    git-svn-id: file:///DYFS/svn/xiaobai.cldsvc/trunk@187 ffb7e7ae-e771-4db6-8443-a1b56181394b

---
+++ app/models/SwmgrClientPackage.php
@@ -0,0 +1,48 @@
+<?php
+namespace DYPA\Models;
+class SwmgrClientPackage extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $caption;
+
+    /**
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     *
+     * @var string
+     */
+    public $identifyingNumber;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $packageCode;
+
+    /**
+     *
+     * @var string
+     */
+    public $packageName;
+
+}
---
+++ app/models/SwmgrUserPackage.php
@@ -0,0 +1,55 @@
+<?php
+namespace DYPA\Models;
+class SwmgrUserPackage extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var integer
+     */
+    public $uid;
+
+    /**
+     *
+     * @var integer
+     */
+    public $pkgid;
+
+    /**
+     *
+     * @var integer
+     */
+    public $installDate;
+
+
+    /**
+     *
+     * @var string
+     */
+    public $installLocation;
+
+    /**
+     *
+     * @var string
+     */
+    public $installSource;
+
+    /**
+     *
+     * @var string
+     */
+    public $localPackage;
+
+    /**
+     *
+     * @var string
+     */
+    public $packageCache;
+
+}
